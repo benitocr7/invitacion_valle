61fd0019c4a8e1e0ddc74e1522199b9ea7b4b091
benitocr7 <edmorales2005@gmail.com>
2026-09-07 16:14:28 -0500

    Use reusable HTML template for general invitation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import CartaInteractiva from '../src/components/CartaInteractiva';
+import InvitacionPersonal, { type DatosInvitacion } from "../src/components/InvitacionPersonal";
+
+const invitacionGeneral: DatosInvitacion = {
+  evento: "Inauguración",
+  iglesia: "Iglesia",
+  sede: "Valle",
+  fecha: "Sábado, 12 de septiembre de 2026",
+  hora: "08H00",
+  horaDetalle: "8 de la mañana",
+  lugar: "Lugar:",
+  direccion: "Valle – Loja, calles Cuenca y Chone",
+  imagenIglesia: "/foto_iglesia.png",
+};
 
 export default function Home() {
-  return (
-    <main>
-      <CartaInteractiva />
-    </main>
-  );
+  return <InvitacionPersonal datos={invitacionGeneral} />;
 }
